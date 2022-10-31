dd53adb6be93a4481ec4d1722612ade5df4de9fb
DA1YAYUAN <836919594@qq.com>
2022-10-31 17:28:40 +0800

    Hdfs client commit

--- java/HdfsClient/src/main/java/com/daiyayuan/hdfs/HdfsClient.java
+++ java/HdfsClient/src/main/java/com/daiyayuan/hdfs/HdfsClient.java
@@ -4,6 +4,8 @@ package com.daiyayuan.hdfs;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -14,20 +16,33 @@ import java.net.URISyntaxException;
  * HDFS客户端
  */
 public class HdfsClient {
-    /**
-     * 创建文件夹
-     */
-    @Test
-    public void mkdir() throws URISyntaxException, IOException {
+
+    private FileSystem fileSystem;
+
+    @Before
+    public void init() throws URISyntaxException, IOException, InterruptedException {
         // name node的通讯端口
         URI uri = new URI("hdfs://hadoop102:8020");
         // 配置文件
         Configuration configuration = new Configuration();
+        // 用户
+        String user = "daiyayuan";
         // 获取客户端对象
-        FileSystem fileSystem = FileSystem.get(uri, configuration);
-        // 创建文件夹
-        fileSystem.mkdirs(new Path("/xiyou/huaguoshan"));
+        fileSystem = FileSystem.get(uri, configuration, user);
+    }
+
+    @After
+    public void close() throws IOException {
         // 关闭资源
         fileSystem.close();
+    }
+
+    /**
+     * 创建文件夹
+     */
+    @Test
+    public void mkdir() throws IOException {
+        // 创建文件夹
+        fileSystem.mkdirs(new Path("/xiyou/huaguoshan"));
     }
 }
